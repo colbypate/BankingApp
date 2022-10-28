77269d79e50418ca22cb3a57b3dfd8cdaf6fdfe7
Colby Pate <cbpate08@gmail.com>
2022-10-27 20:21:24 -0500

    Add project files.

---
+++ BankingApp/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace BankingApp.Models
+{
+    public class Transaction
+    {
+        public int transactionid { get; set; }
+        public int accountid { get; set; }
+        public string type { get; set; }
+        public int amount { get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
